d64913236a064e8a5851351a960196a5a42d9e7d
Angela Xie <angelaxie2015@gmail.com>
2020-12-20 14:22:08 +0000

    Turn on and off responding

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import os
 import requests
 import json 
 import random 
-from relpit import db
+from replit import db
 
 #discord.py library is used in this project. 
 #os provides a portable way of using operating system dependent functionality
 #requests allows us to request from http stuff
 #json JSON is a lightweight format for storing and transporting data, often used when data is sent from a server to a web page
+
+client = discord.Client()
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"] #a list of sad words, if the bot sees one, it will respond
+starter_encouragements = ['cheer up!', "Hang in there.", "You are better than what you think you are!"]
+
+if "responding" not in db.keys():
+  db["responding"] = True
 
 #request quote from api
 def get_quote(): 
@@ -32,9 +39,6 @@ def delete_encouragement(index):
     del encouragements[index]
     db["encouragements"] = encouragements
 
-client = discord.Client()
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"] #a list of sad words, if the bot sees one, it will respond
-starter_encouragements = ['cheer up!', "Hang in there.", "You are better than what you think you are!"]
 #register library 
 #A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
 @client.event 
@@ -54,13 +58,14 @@ async def on_message(message):
     quote = get_quote()
     await message.channel.send(quote)
 
-  options = starter_encouragements
-  if "encouragements" in db.keys():
-    options = options + db["encouragements"]
+  if db["responding"]:
+    options = starter_encouragements
+    if "encouragements" in db.keys():
+      options = options + db["encouragements"]
 
-  #check if it has any of the words in the sadwords list
-  if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(options))
+    #check if it has any of the words in the sadwords list
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
 
   if msg.startswith("$new"):
     encouraging_message = msg.split("$new ", 1)[1]
@@ -69,16 +74,26 @@ async def on_message(message):
 
   if msg.startswith('$del'):
     encouragements = []
-    if "encouragments" in db.keys():
+    if "encouragements" in db.keys():
       index = int(msg.split("$del", 1)[1])
       delete_encouragement(index)
-      encouragements = db["encouragemtns"]
+      encouragements = db["encouragements"]
     await message.channel.send(encouragements)
 
+  if msg.startswith('$lists'):
+    encouragements = []
+    if "encouragements" in db.keys():
+      encouragements = db["encouragements"]
+    await message.channel.send(encouragements)
 
-  
+  if msg.startswith("$responding"):
+    val = msg.split("$responding ", 1)[1]
 
-
-
+    if val.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("responding is on")
+    else:
+      db["responding"] = False
+      await message.channel.send("responding is off")
 
 client.run(os.getenv('TOKEN'))
