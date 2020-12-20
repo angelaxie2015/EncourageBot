1d772848c0dd64dda6ca29ba1662199a9ef49cd6
Angela Xie <angelaxie2015@gmail.com>
2020-12-20 13:26:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import discord 
+
+#discord.py library is used in this project. 
+
+client = discord.Client()
+
+#register library 
+@client.event 
+
+#A callback function is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+
